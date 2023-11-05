e647b532fa9515c55bb725d522b743de7e04b80b
tmorse01 <74744091+tmorse01@users.noreply.github.com>
2023-11-04 20:50:07 -0700

    ch16

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,12 @@
 import AcmeLogo from "@/app/ui/acme-logo";
 import { lusitana } from "@/app/ui/fonts";
 import Image from "next/image";
-
 import Link from "next/link";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Dashboard | Acme Dashboard",
+};
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
